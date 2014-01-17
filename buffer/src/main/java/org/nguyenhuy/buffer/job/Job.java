ce8c5ceca5b929280cd6f03a919b0a1f7ab0f3ea
Huy Nguyen <allforone1511@gmail.com>
2014-01-17 20:50:21 +0200

    Change retry limit from 20 down to 3, to fail fast.

--- buffer/src/main/java/org/nguyenhuy/buffer/job/Job.java
+++ buffer/src/main/java/org/nguyenhuy/buffer/job/Job.java
@@ -10,6 +10,8 @@ import javax.inject.Inject;
  * Created by nguyenthanhhuy on 1/14/14.
  */
 public abstract class Job extends com.path.android.jobqueue.Job {
+    private static final int DEFAULT_RETRY_LIMIT =  3;
+
     @Inject
     transient Bus bus;
     @Inject
@@ -17,5 +19,10 @@ public abstract class Job extends com.path.android.jobqueue.Job {
 
     protected Job(Params params) {
         super(params);
+    }
+
+    @Override
+    protected int getRetryLimit() {
+        return DEFAULT_RETRY_LIMIT;
     }
 }
